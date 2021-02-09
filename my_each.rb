f9d310a4f88fc36b7b990904d1809e6c2fb1c8b7
Alexander Gould <alexander.gould94@gmail.com>
2021-02-09 11:08:21 -0800

    commit tree dcfa2352153bb848fd710e30fba3e636799170f5

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
-  # code here
+# def my_each(collection) do # put argument(s) here
+#   i=0
+#     while i < collection.length
+#       putcolletion[i]
+#       i += 1
+#     end
+#   end
+#   collection
+# end
+
+def my_each(collection) 
+  i = 0
+  while i < collection.length
+    yield collection[i] 
+    i = i+1
+  end
+  collection
 end
+
+
+
